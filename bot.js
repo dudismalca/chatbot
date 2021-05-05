10800ddf6c99dd98f8efe7ac9223b47df1a4698b
dudismalca <eduardo.malca@hotmail.com>
2021-05-05 01:44:20 -0300

    Atualizacao

--- bot.js
+++ bot.js
@@ -1,48 +1,31 @@
 console.log("FERRAMENTAS ESTÃO OK");
 
 const tmi = require('tmi.js');
-const NOME_DO_BOT = 'jokerpitter01'
-const canal = 'teamjokerzskulll'
-const TOKEN = 'oauth:pj0wtmr3433qd0m1sbsknfxlzu9vod'
+
 
 const opts = {
-    identity: {
-    username: NOME_DO_BOT,
-    password: TOKEN
+    options: {
+        debug: false
     },
-    channels: [ canal ]
-  };
+    connection: {
+        reconnect: true,
+        secure: true
+    },
+  identity: {
+    username: 'teamjokerz' ,
+    password: 'oauth:stixhpnimzh8brfu8jitish4xcrm6j'
+  },
+  channels: [ 'liminhag0d', 'brnwowzk1', 'gaules', 'saullo', 'murilo_rt', 'cachorro1337c', 'bt0tv', 'tiburciand0', 'sksfps1', 'mch_agg', 'deercheerup', 'jument1nho', 'danoc0', 'loud_coringa', 'CSRfps'
+  ]
+};
 
-  //intercepta mensagem do chat
-function mensagemChegou(alvo, contexto, mensagem, ehBot) {
-    if (ehBot) {
-      return; //se for mensagens do nosso bot ele não faz nada
-    } 
-  
-  const nomeDoComando = mensagem.trim();// remove espaço em branco da mensagem para verificar o comando
-  // checando o nosso comando
-    if (nomeDoComando === '!comandoUM') {
-    console.log(`* Foi Executado um comando ${nomeDoComando}`);
-    client.say(alvo, `* Você pediu para executar o comando ${nomeDoComando}`);
-    } else if (nomeDoComando === '!comandoDOIS') {
-    console.log(`* Foi Executado o comando ${nomeDoComando}`);
-    client.say(alvo, `* Você pediu para executar o comando ${nomeDoComando}`);
-    } else {
-    console.log(`* Não conheço o comando ${nomeDoComando}`);
-    client.say(alvo, `* Não existe o comando ${nomeDoComando}`);
-    }
-  }
-  
-  function entrouNoChatDaTwitch(endereco, porta) {
-    console.log(`* Bot entrou no endereço ${endereco}:${porta}`);
-    client.say(canal, `Cheguei carai`);
-  }
-
-  // Cria um cliente tmi com  nossas opções
+// Cria um cliente tmi com  nossas opções
 const client = new tmi.client(opts);
 
-// Registra nossas funções
-client.on('message', mensagemChegou);
-client.on('connected', entrouNoChatDaTwitch);
 // Connecta na Twitch:
-client.connect();
+client.connect()
+.then((data) => {
+    console.log('connectado', data);
+}).catch((err) => {
+    console.log('Erro connect', err);
+});
